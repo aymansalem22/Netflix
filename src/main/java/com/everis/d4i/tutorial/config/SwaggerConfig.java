2f00b54d13face152c8318f4ae889939c6e8a475
aymansalem22 <s.aymansalem@gmail.com>
2021-09-18 14:45:04 +0200

    refactor swaggerconfig

--- src/main/java/com/everis/d4i/tutorial/config/SwaggerConfig.java
+++ src/main/java/com/everis/d4i/tutorial/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -37,7 +38,8 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).groupName("Netflix").apiInfo(apiInfo()).select()
-				.paths(regex(".*" + "/.*")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.everis.d4i.tutorial")).paths(regex(".*" + "/.*"))
+				.build();
 	}
 
 	private ApiInfo apiInfo() {
